343e2a4d1ef873f1e8225599abffefea65ad04ff
mohamed ahmed <ahmed2844ah@gmail.com>
2026-02-28 14:22:17 +0200

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,11 @@ export default defineConfig(({ mode }) => {
         port: 3000,
         host: '0.0.0.0',
       },
+      preview: {
+        port: 3000,
+        host: '0.0.0.0',
+        allowedHosts: true,
+      },
       plugins: [
         react(),
         tailwindcss(),
